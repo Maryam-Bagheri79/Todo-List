510bb24bc84e464ab8f7e253cb2ba56067fb32e9
Maryam-Bagheri79 <m.bagheri7939@gmail.com>
2021-11-24 17:52:52 +0330

    Half Todo

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 //! TodoList : 80
 
+let todoList = [
+    { id: 1, nameTod: 'study', Condition: 'undone' },
+    { id: 2, nameTod: 'programming', Condition: 'done' },
+    { id: 3, nameTod: 'exam', Condition: 'undone' }
+]
+
+let userRequest = prompt("گزینه مورد نظر را انتخاب کنید: \n1.اضافه کردن \n2.حذف کردن \n3.انجام دادن")
+
+if (userRequest === '1') {
+    let addTodo = prompt("فعالیتی را به لیست اضافه کنید");
+    { }
+}
